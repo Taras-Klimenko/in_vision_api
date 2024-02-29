85d834752bd418a07a3bbe4aa8aed61fe89d8108
Taras-Klimenko <tarasklimenko.1989@gmail.com>
2024-02-29 12:48:43 +0300

    Database connection fix

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const db = knex({
     port: 5432,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PW,
-    database: process.env.DATABASE_PW,
+    database: process.env.DATABASE_DB,
   },
 });
 
